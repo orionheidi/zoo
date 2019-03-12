364979cb792e92667a4f1d33d0b72ca3909ed1b4
FilipNedovic <nedovic.filip@gmail.com>
2019-03-12 19:09:59 +0100

    third

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -5,10 +5,11 @@
     <th>Ime</th>
     <th>Datum</th>
    </tr>
-  <tr  v-for="(animal) in animals" :key="animal"> 
-     <td> {{animal.specie}}</td>
-    <td> {{animal.name}}</td>
-     <td> {{animal.birthday ? animal.birthday :'nepoznato'}}</td>
+  <tr  v-for="(animal,index) in animals" :key="animal"> 
+     <td> {{animal.specie}}  {{ index }}</td>
+    <td> {{animal.name}}   {{ index }}</td>
+     <td> {{animal.birthday ? animal.birthday :'nepoznato'}}   {{ index }}</td>
+    <button @click="removeItem(index)">Delete</button>
   </tr>
 </table>
 </template>
@@ -28,6 +29,17 @@ export default {
        
        }
    },
+
+    methods: {
+
+     removeItem(index) {
+      if (this.deleteWithSplice) {
+        this.animals.splice(index, 1);
+      } else {
+        this.$delete(this.animals, index);
+      }
+    },
+   }
 
 }
 </script>
