977f7c5246aa95cc1660a4e2503d5490485e4b12
FilipNedovic <nedovic.filip@gmail.com>
2019-03-12 20:22:07 +0100

    6

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -8,6 +8,10 @@
   <input type="text" v-model="animal.name" />
    <label>Rodjendan:</label>
     <input type="text" v-model="animal.birthday">
+  <label>Sektor:</label>
+  <select v-model="animal.sector">
+  <option v-for="(sector) in sectors" :key="sector" :value="sector">{{sector}}</option>
+  </select>
   </div>
 
     <button type="submit">add</button>
@@ -22,6 +26,7 @@
      <td> {{animal.specie}}  {{ index }}</td>
     <td> {{animal.name}}   {{ index }}</td>
      <td> {{animal.birthday ? animal.birthday :'nepoznato'}}   {{ index }}</td>
+     <td> {{animal.sector}}   {{ index }}</td>
     <button @click="removeItem(index)">Delete</button>
      <button @click="moveToTop(index)">MoveToTop</button>
   </tr>
@@ -34,19 +39,29 @@ export default {
   data () {
        return{
         animals: [
-          {specie:"Tiger", name:"Nensy", birthday:"20.03.2010"},
-          {specie:"Bear", name:"Dundi", birthday:"23.082015"},
-          {specie:"Seagle", name:"Lord", birthday:"30.05.2013"},
-          {specie:"Donky", name:"Baba", birthday:""},
-          {specie:"Octopod", name:"Sisy", birthday:"10.02.2018"},
+          {specie:"Tiger", name:"Nensy", birthday:"20.03.2010",sector:"opanse zivotinje"},
+          {specie:"Bear", name:"Dundi", birthday:"23.082015",sector:"spavalice"},
+          {specie:"Seagle", name:"Lord", birthday:"30.05.2013",sector:"ptice"},
+          {specie:"Donky", name:"Baba", birthday:"",sector:"domace"},
+          {specie:"Octopod", name:"Sisy", birthday:"10.02.2018",sector:"vodene"},
 
         ],
 
         animal:{
         name: '',
         specie: '',
-        birthday: ''
-        }
+        birthday: '',
+        sector: ''
+        },
+
+        sectors: [
+          "opanse zivotinje",
+          "spavalice",
+          "ptice",
+          "domace",
+          "vodene"
+
+        ]
   
        
        }
